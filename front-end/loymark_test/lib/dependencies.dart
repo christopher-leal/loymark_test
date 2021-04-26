00fa9d537fea8bc8cf9efeca20d0197cfa4e1969
Christopher Leal <chris_t_o@hotmail.com>
2021-04-26 17:17:41 -0500

    activities usecase added to di

--- front-end/loymark_test/lib/dependencies.dart
+++ front-end/loymark_test/lib/dependencies.dart
@@ -1,13 +1,19 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:loymark_test/data/repositories/user_service.dart';
+import 'package:loymark_test/data/repositories/activities_services.dart';
+import 'package:loymark_test/data/repositories/users_service.dart';
+import 'package:loymark_test/domain/repositories/activities_repository.dart';
 import 'package:loymark_test/domain/repositories/users_repository.dart';
 import 'package:loymark_test/domain/usecases/delete_user_usecase.dart';
 import 'package:loymark_test/domain/usecases/get_users_usecase.dart';
 
+import 'domain/usecases/get_activities_usecase.dart';
+
 List<RepositoryProvider> buildRepositories = [
   // repositories
-  RepositoryProvider<UsersRepository>(create: (context) => UserService()),
+  RepositoryProvider<UsersRepository>(create: (context) => UsersService()),
+  RepositoryProvider<ActivitiesRepository>(create: (context) => ActivitiesService()),
   // usecases
   RepositoryProvider<GetUsersUseCase>(create: (context) => GetUsersUseCase(context.read())),
   RepositoryProvider<DeleteUserUseCase>(create: (context) => DeleteUserUseCase(context.read())),
+  RepositoryProvider<GetActivitiesUseCase>(create: (context) => GetActivitiesUseCase(context.read())),
 ];
