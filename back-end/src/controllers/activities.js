636899938fb8efcf41cfa0df6a1b7e9364bc118a
Christopher Leal <chris_t_o@hotmail.com>
2021-04-26 17:13:22 -0500

    include clause added to query

--- back-end/src/controllers/activities.js
+++ back-end/src/controllers/activities.js
@@ -3,14 +3,13 @@ const { STATUS_CODES, defaultResponses } = require('../utils/defaultResponses');
 const utils = require('./../utils/utils');
 
 const getActivities = async (req, res) => {
-    const data = await utils.getItemsWithPagination(models.Activity, req.body);
+    const data = await utils.getItemsWithPagination(models.Activity, req.body, {}, 'user');
     return res.status(data.statusCode).json(data);
 };
 
 const getActivitiesByUser = async (req, res) => {
     const { userId } = req.body;
     if (!userId) return res.status(STATUS_CODES.BAD_REQUEST).json(defaultResponses.BadRequest);
-
     const data = await utils.getItemsWithPagination(models.Activity, req.body, { userId });
     return res.status(data.statusCode).json(data);
 };
