faf110181fd260e5169d319bf92519596242f5cb
Christopher Leal <chris_t_o@hotmail.com>
2021-04-26 02:20:14 -0500

    dependency injection file added

---
+++ front-end/loymark_test/lib/dependencies.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:loymark_test/data/repositories/user_service.dart';
+import 'package:loymark_test/domain/repositories/users_repository.dart';
+import 'package:loymark_test/domain/usecases/get_users_usecase.dart';
+
+List<RepositoryProvider> buildRepositories = [
+  // repositories
+  RepositoryProvider<UsersRepository>(create: (context) => UserService()),
+  // usecases
+  RepositoryProvider<GetUsersUseCase>(create: (context) => GetUsersUseCase(context.read())),
+];
