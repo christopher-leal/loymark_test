2482471107d34a0d63fe028460a71b0547d7bf24
Christopher Leal <chris_t_o@hotmail.com>
2021-04-26 01:24:29 -0500

    response in server modified

--- back-end/server.js
+++ back-end/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { defaultResponses, STATUS_CODES } = require('./src/utils/defaultResponses');
 const app = express();
 require('dotenv').config();
 
@@ -12,6 +13,6 @@ app.use('/user', require('./src/routes/users'));
 app.use('/activity', require('./src/routes/activities'));
 
 app.get('/', (req, res) => {
-    res.status(200).json({ success: true, status: 200 })
+    res.status(STATUS_CODES.OK).json(defaultResponses.Success)
 });
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
