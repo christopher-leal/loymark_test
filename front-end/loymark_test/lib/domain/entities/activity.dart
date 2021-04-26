1ed917b6e94c9b121ada1f55c90498a55aabfb2d
Christopher Leal <chris_t_o@hotmail.com>
2021-04-26 17:14:46 -0500

    activity now has user

--- front-end/loymark_test/lib/domain/entities/activity.dart
+++ front-end/loymark_test/lib/domain/entities/activity.dart
@@ -4,29 +4,27 @@
 
 import 'dart:convert';
 
+import 'package:loymark_test/domain/entities/user.dart';
+
 Activity activityFromJson(String str) => Activity.fromJson(json.decode(str) as Map<String, dynamic>);
 
 String activityToJson(Activity data) => json.encode(data.toJson());
 
 class Activity {
-  Activity({
-    this.id,
-    this.userId,
-    this.activity,
-    this.createDate,
-  });
+  Activity({this.id, this.userId, this.activity, this.createDate, this.user});
   factory Activity.fromJson(Map<String, dynamic> json) => Activity(
         id: json['id'] as int,
         userId: json['userId'] as int,
         activity: json['activity'] as String,
         createDate: json['createDate'] != null ? DateTime.parse(json['createDate'] as String) : null,
+        user: User.fromJson(json['user'] as Map<String, dynamic>),
       );
 
   final int id;
   final int userId;
   final String activity;
   final DateTime createDate;
-
+  final User user;
   Map<String, dynamic> toJson() => {
         'id': id,
         'userId': userId,
