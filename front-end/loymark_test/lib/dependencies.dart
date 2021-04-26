d92f54094ce32a303a022c673183bdff6d65f614
Christopher Leal <chris_t_o@hotmail.com>
2021-04-26 13:27:44 -0500

    use case added to dependency injection

--- front-end/loymark_test/lib/dependencies.dart
+++ front-end/loymark_test/lib/dependencies.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loymark_test/data/repositories/user_service.dart';
 import 'package:loymark_test/domain/repositories/users_repository.dart';
+import 'package:loymark_test/domain/usecases/delete_user_usecase.dart';
 import 'package:loymark_test/domain/usecases/get_users_usecase.dart';
 
 List<RepositoryProvider> buildRepositories = [
@@ -8,4 +9,5 @@ List<RepositoryProvider> buildRepositories = [
   RepositoryProvider<UsersRepository>(create: (context) => UserService()),
   // usecases
   RepositoryProvider<GetUsersUseCase>(create: (context) => GetUsersUseCase(context.read())),
+  RepositoryProvider<DeleteUserUseCase>(create: (context) => DeleteUserUseCase(context.read())),
 ];
