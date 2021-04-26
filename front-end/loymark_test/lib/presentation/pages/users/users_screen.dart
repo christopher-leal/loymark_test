128167cf474f486f9f1f78546fdc3f47e511d5b2
Christopher Leal <chris_t_o@hotmail.com>
2021-04-26 13:57:12 -0500

    user screen progress

--- front-end/loymark_test/lib/presentation/pages/users/users_screen.dart
+++ front-end/loymark_test/lib/presentation/pages/users/users_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:loymark_test/core/utils/utils.dart';
+import 'package:loymark_test/presentation/pages/users/user_history_cubit.dart';
+import 'package:loymark_test/presentation/pages/users/user_screen.dart';
 import 'package:loymark_test/presentation/pages/users/users_cubit.dart';
+import 'package:loymark_test/presentation/widgets/user_data_search.dart';
+import 'package:loymark_test/presentation/widgets/user_list_item.dart';
 
 class UsersScreen extends StatefulWidget {
   @override
@@ -18,8 +23,9 @@ class _UsersScreenState extends State<UsersScreen> {
   }
 
   void onListen() {
+    final userCubit = context.read<UsersCubit>();
     if (_scrollController.offset == _scrollController.position.maxScrollExtent) {
-      context.read<UsersCubit>().getUsers();
+      if (!userCubit.isAllData) userCubit.getUsers();
     }
   }
 
@@ -30,42 +36,113 @@ class _UsersScreenState extends State<UsersScreen> {
     super.dispose();
   }
 
+  Future<bool> _dismissConfirm(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmar'),
+          content: const Text('¿Deseas eliminar este usuario?'),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () => Navigator.of(context).pop(false),
+              child: const Text('Cancelar'),
+            ),
+            FlatButton(onPressed: () => Navigator.of(context).pop(true), child: const Text('Borrar')),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<UsersCubit, UsersState>(
-        builder: (context, state) {
-          if (state.users.isEmpty && state.isLoading) return const Center(child: CircularProgressIndicator());
-          return ListView.builder(
-            physics: const BouncingScrollPhysics(),
-            itemCount: state.users.length + 1,
-            controller: _scrollController,
-            itemBuilder: (BuildContext context, int index) {
-              if (index < state.users.length) {
-                final user = state.users[index];
-                return ListTile(
-                  leading: Text(user.id.toString()),
-                  title: Text(user.name),
-                  onTap: () {},
+    return BlocProvider(
+      create: (context) => UserHistoryCubit(context.read()),
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Usuarios'),
+          actions: [
+            Builder(
+              builder: (context) {
+                return IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () async {
+                    final selectedUser = await showSearch(
+                      context: context,
+                      delegate: UsersDataSearch(BlocProvider.of<UserHistoryCubit>(context)),
+                    );
+                    if (selectedUser != null) {
+                      context.read<UserHistoryCubit>().addToHistory(selectedUser);
+                      Utils.navigateTo(context, UserScreen(user: selectedUser));
+                    }
+                  },
                 );
-              } else if (context.read<UsersCubit>().isAllData) {
-                return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    child: Center(
-                      child: Text(
-                        'No hay más información para mostrar',
-                        style: Theme.of(context).textTheme.bodyText1,
+              },
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+        body: BlocBuilder<UsersCubit, UsersState>(
+          builder: (context, state) {
+            if (state.users.isEmpty && state.isLoading) return const Center(child: CircularProgressIndicator());
+            return ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: state.users.length + 1,
+              controller: _scrollController,
+              itemBuilder: (BuildContext context, int index) {
+                if (index < state.users.length) {
+                  final user = state.users[index];
+                  return Dismissible(
+                    key: ValueKey(user.id),
+                    direction: DismissDirection.endToStart,
+                    background: Container(
+                      color: Colors.red,
+                      alignment: Alignment.centerRight,
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                        ),
                       ),
-                    ));
-              } else {
-                return const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 12),
-                  child: Center(child: CupertinoActivityIndicator()),
-                );
-              }
-            },
-          );
-        },
+                    ),
+                    onDismissed: (direction) {
+                      context.read<UsersCubit>().deleteUser(user.id);
+                    },
+                    confirmDismiss: (DismissDirection direction) async {
+                      return await _dismissConfirm(context);
+                    },
+                    child: UserListItem(
+                      user: user,
+                      onTap: () {
+                        Utils.navigateTo(context, UserScreen(user: user));
+                      },
+                    ),
+                  );
+                } else if (context.read<UsersCubit>().isAllData) {
+                  return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      child: Center(
+                        child: Text(
+                          'No hay más información para mostrar',
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ));
+                } else {
+                  return const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 12),
+                    child: Center(child: CupertinoActivityIndicator()),
+                  );
+                }
+              },
+            );
+          },
+        ),
       ),
     );
   }
