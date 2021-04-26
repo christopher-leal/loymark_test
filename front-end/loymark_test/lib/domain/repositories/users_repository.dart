d20df2639013049917c2e735d4f9137374256b5b
Christopher Leal <chris_t_o@hotmail.com>
2021-04-26 13:29:05 -0500

    searchUser repository response changed

--- front-end/loymark_test/lib/domain/repositories/users_repository.dart
+++ front-end/loymark_test/lib/domain/repositories/users_repository.dart
@@ -3,7 +3,7 @@ import 'package:loymark_test/domain/entities/user.dart';
 
 abstract class UsersRepository {
   Future<ApiListResponse<User>> getUsers(int offset);
-  Future<ApiListResponse<User>> searchUsers(String find);
+  Future<List<User>> searchUsers(String find);
   Future<bool> upsertUser();
   Future<bool> deleteUser(int id);
 }
