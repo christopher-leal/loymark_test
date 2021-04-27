9bbe769481348904d9484383c211a618ff83b764
Christopher Leal <chris_t_o@hotmail.com>
2021-04-26 23:23:22 -0500

    requests added to the servicesa

--- front-end/loymark_test/lib/data/repositories/activities_services.dart
+++ front-end/loymark_test/lib/data/repositories/activities_services.dart
@@ -1,16 +1,18 @@
-import 'package:loymark_test/core/dio.dart';
+import 'package:loymark_test/core/environment.dart';
 import 'package:loymark_test/domain/entities/activity.dart';
 import 'package:loymark_test/data/models/api_list_response.dart';
 import 'package:loymark_test/domain/entities/api_response.dart';
 import 'package:loymark_test/domain/repositories/activities_repository.dart';
+import 'package:requests/requests.dart';
 
 class ActivitiesService extends ActivitiesRepository {
   @override
   Future<ApiListResponse<Activity>> getActivities(int offset, {int attempts = 0}) async {
     try {
-      final response = await dio.post('/activity/getActivities', data: {'offset': offset, 'order': 'createDate'});
+      final response = await Requests.post('${Environment.apiUrl}/activity/getActivities',
+          body: {'offset': offset, 'order': 'createDate'}, bodyEncoding: RequestBodyEncoding.JSON);
       if (response.statusCode == 200) {
-        final apiResponse = ApiResponse.fromJson(response.data as Map<String, dynamic>);
+        final apiResponse = ApiResponse.fromJson(response.json() as Map<String, dynamic>);
         if (apiResponse.success) {
           final totalItems = apiResponse.data['totalItems'] as int;
           final items = apiResponse.data['items'] as Iterable;
@@ -28,9 +30,10 @@ class ActivitiesService extends ActivitiesRepository {
   @override
   Future<List<Activity>> getActivitiesByUser(int userId, int offset, {int attempts = 0}) async {
     try {
-      final response = await dio.post('/activity/getActivitiesByUser', data: {'offset': offset, 'userId': userId, 'order': 'createDate'});
+      final response = await Requests.post('${Environment.apiUrl}/activity/getActivitiesByUser',
+          body: {'offset': offset, 'userId': userId, 'order': 'createDate'}, bodyEncoding: RequestBodyEncoding.JSON);
       if (response.statusCode == 200) {
-        final apiResponse = ApiResponse.fromJson(response.data as Map<String, dynamic>);
+        final apiResponse = ApiResponse.fromJson(response.json() as Map<String, dynamic>);
         if (apiResponse.success) {
           final items = apiResponse.data['items'] as Iterable;
           final users = items.map((item) => Activity.fromJson(item as Map<String, dynamic>)).toList();
--- front-end/loymark_test/lib/data/repositories/users_service.dart
+++ front-end/loymark_test/lib/data/repositories/users_service.dart
@@ -1,16 +1,17 @@
-import 'package:loymark_test/core/dio.dart';
+import 'package:loymark_test/core/environment.dart';
 import 'package:loymark_test/data/models/api_list_response.dart';
 import 'package:loymark_test/domain/entities/api_response.dart';
 import 'package:loymark_test/domain/entities/user.dart';
 import 'package:loymark_test/domain/repositories/users_repository.dart';
+import 'package:requests/requests.dart';
 
 class UsersService extends UsersRepository {
   @override
   Future<ApiListResponse<User>> getUsers(int offset, {int attempts = 0}) async {
     try {
-      final response = await dio.post('/user/getUsers', data: {'offset': offset});
+      final response = await Requests.post('${Environment.apiUrl}/user/getUsers', body: {'offset': offset}, bodyEncoding: RequestBodyEncoding.JSON);
       if (response.statusCode == 200) {
-        final apiResponse = ApiResponse.fromJson(response.data as Map<String, dynamic>);
+        final apiResponse = ApiResponse.fromJson(response.json() as Map<String, dynamic>);
         if (apiResponse.success) {
           final totalItems = apiResponse.data['totalItems'] as int;
           final items = apiResponse.data['items'] as Iterable;
@@ -28,9 +29,9 @@ class UsersService extends UsersRepository {
   @override
   Future<List<User>> searchUsers(String find, {int attempts = 0}) async {
     try {
-      final response = await dio.post('/user/searchUser', data: {'find': find});
+      final response = await Requests.post('${Environment.apiUrl}/user/searchUser', body: {'find': find}, bodyEncoding: RequestBodyEncoding.JSON);
       if (response.statusCode == 200) {
-        final apiResponse = ApiResponse.fromJson(response.data as Map<String, dynamic>);
+        final apiResponse = ApiResponse.fromJson(response.json() as Map<String, dynamic>);
         if (apiResponse.success) {
           final items = apiResponse.data['items'] as Iterable;
           final users = items.map((item) => User.fromJson(item as Map<String, dynamic>)).toList();
@@ -47,9 +48,10 @@ class UsersService extends UsersRepository {
   @override
   Future<bool> upsertUser(User user, {int attempts = 0}) async {
     try {
-      final response = await dio.post('/user/upsertUser', data: user.toJson());
+      final response = await Requests.post('${Environment.apiUrl}/user/upsertUser', body: user.toJson(), bodyEncoding: RequestBodyEncoding.JSON);
+      // final response = await Requests.post('/user/upsertUser', data: user.toJson());
       if (response.statusCode == 200) {
-        final apiResponse = ApiResponse.fromJson(response.data as Map<String, dynamic>);
+        final apiResponse = ApiResponse.fromJson(response.json() as Map<String, dynamic>);
         return apiResponse.success;
       }
     } catch (e) {
@@ -62,9 +64,9 @@ class UsersService extends UsersRepository {
   @override
   Future<bool> deleteUser(int id, {int attempts = 0}) async {
     try {
-      final response = await dio.get('/user/delete/$id');
+      final response = await Requests.get('${Environment.apiUrl}/user/delete/$id');
       if (response.statusCode == 200) {
-        final apiResponse = ApiResponse.fromJson(response.data as Map<String, dynamic>);
+        final apiResponse = ApiResponse.fromJson(response.json() as Map<String, dynamic>);
         return apiResponse.success;
       }
     } catch (e) {
