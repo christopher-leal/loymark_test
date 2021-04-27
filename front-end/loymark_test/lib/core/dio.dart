149b3a2446fcb8b1db53aaff093479d5fe4933b7
Christopher Leal <chris_t_o@hotmail.com>
2021-04-26 23:22:35 -0500

    dio file removed

--- front-end/loymark_test/lib/core/dio.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-import 'package:dio/dio.dart';
-import 'package:loymark_test/core/environment.dart';
-
-final dio = Dio(BaseOptions(
-  baseUrl: Environment.apiUrl,
-  contentType: ContentType.json.toString(),
-));
