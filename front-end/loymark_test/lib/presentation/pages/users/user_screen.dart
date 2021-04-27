9a88bd3bd13fa825febd6c175d1b47bf8a5f22a9
Christopher Leal <chris_t_o@hotmail.com>
2021-04-26 21:26:30 -0500

    user screen modified

--- front-end/loymark_test/lib/presentation/pages/users/user_screen.dart
+++ front-end/loymark_test/lib/presentation/pages/users/user_screen.dart
@@ -4,6 +4,7 @@ import 'package:loymark_test/core/utils/utils.dart';
 import 'package:loymark_test/domain/entities/activity.dart';
 import 'package:loymark_test/domain/entities/user.dart';
 import 'package:loymark_test/presentation/pages/activities/activities_cubit.dart';
+import 'package:loymark_test/presentation/pages/users/upsert_user_screen.dart';
 
 class UserScreen extends StatelessWidget {
   const UserScreen({Key key, this.user}) : super(key: key);
@@ -45,76 +46,31 @@ class UserScreen extends StatelessWidget {
         appBar: AppBar(
           centerTitle: true,
           title: const Text('Usuario'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.edit),
+              onPressed: () {
+                Utils.navigateTo(context, UpsertUserScreen(user: user));
+              },
+            )
+          ],
         ),
         body: SafeArea(
           child: Column(
             children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Información',
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                  Wrap(
-                    children: [
-                      const Text('Nombre completo: ', style: TextStyle(fontSize: 16.0)),
-                      Text(
-                        user.fullName.toUpperCase(),
-                        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),
-                      ),
-                    ],
-                  ),
-                  Wrap(
-                    children: [
-                      const Text('Correo: ', style: TextStyle(fontSize: 16.0)),
-                      Text(
-                        user.email,
-                        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),
-                      ),
-                    ],
-                  ),
-                  Wrap(
-                    children: [
-                      const Text('Fecha de nacimiento: ', style: TextStyle(fontSize: 16.0)),
-                      Text(
-                        Utils.formatDateWithoutTime(datetime: user.birthday),
-                        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),
-                      ),
-                    ],
-                  ),
-                  Wrap(
-                    children: [
-                      const Text('Pais: ', style: TextStyle(fontSize: 16.0)),
-                      Text(
-                        user.country,
-                        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),
-                      ),
-                      const SizedBox(width: 12.0),
-                      const Text('Telefono: ', style: TextStyle(fontSize: 16.0)),
-                      Text(
-                        user.phone ?? '-',
-                        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      const Text('Recibir información: ', style: TextStyle(fontSize: 16.0)),
-                      Checkbox(value: user.receiveInfo, onChanged: null),
-                    ],
-                  ),
-                ],
-              ),
+              _UserInformation(user: user),
               Expanded(
                 child: BlocBuilder<ActivitiesCubit, ActivitiesState>(
                   builder: (context, state) {
                     if (state.activities.isEmpty && state.isLoading) return const Center(child: CircularProgressIndicator());
                     return Column(
                       children: [
-                        const Text(
-                          'Actividades',
-                          style: TextStyle(fontSize: 20.0),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8.0),
+                          child: Text(
+                            'Actividades',
+                            style: TextStyle(fontSize: 20.0),
+                          ),
                         ),
                         Expanded(
                           child: ListView.separated(
@@ -136,6 +92,79 @@ class UserScreen extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class _UserInformation extends StatelessWidget {
+  const _UserInformation({
+    Key key,
+    @required this.user,
+  }) : super(key: key);
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          child: Text(
+            'Información',
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ),
+        Wrap(
+          children: [
+            const Text('Nombre completo: ', style: TextStyle(fontSize: 16.0)),
+            Text(
+              user.fullName.toUpperCase(),
+              style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),
+            ),
+          ],
+        ),
+        Wrap(
+          children: [
+            const Text('Correo: ', style: TextStyle(fontSize: 16.0)),
+            Text(
+              user.email,
+              style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),
+            ),
+          ],
+        ),
+        Wrap(
+          children: [
+            const Text('Fecha de nacimiento: ', style: TextStyle(fontSize: 16.0)),
+            Text(
+              Utils.formatDateWithoutTime(datetime: user.birthday),
+              style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),
+            ),
+          ],
+        ),
+        Wrap(
+          children: [
+            const Text('Pais: ', style: TextStyle(fontSize: 16.0)),
+            Text(
+              user.country,
+              style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),
+            ),
+            const SizedBox(width: 12.0),
+            const Text('Telefono: ', style: TextStyle(fontSize: 16.0)),
+            Text(
+              user.phone ?? '-',
+              style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            const Text('Recibir información: ', style: TextStyle(fontSize: 16.0)),
+            Checkbox(value: user.receiveInfo, onChanged: null),
+          ],
+        ),
+      ],
     );
   }
 }
