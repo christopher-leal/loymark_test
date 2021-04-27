16fe0d657b868a150e057ef420c76335daad85e7
Christopher Leal <chris_t_o@hotmail.com>
2021-04-26 21:24:15 -0500

    dependencies modified

--- front-end/loymark_test/lib/dependencies.dart
+++ front-end/loymark_test/lib/dependencies.dart
@@ -4,9 +4,11 @@ import 'package:loymark_test/data/repositories/users_service.dart';
 import 'package:loymark_test/domain/repositories/activities_repository.dart';
 import 'package:loymark_test/domain/repositories/users_repository.dart';
 import 'package:loymark_test/domain/usecases/delete_user_usecase.dart';
+import 'package:loymark_test/domain/usecases/get_countries_usecase.dart';
 import 'package:loymark_test/domain/usecases/get_users_usecase.dart';
 
 import 'domain/usecases/get_activities_usecase.dart';
+import 'domain/usecases/upsert_user_usecase.dart';
 
 List<RepositoryProvider> buildRepositories = [
   // repositories
@@ -16,4 +18,6 @@ List<RepositoryProvider> buildRepositories = [
   RepositoryProvider<GetUsersUseCase>(create: (context) => GetUsersUseCase(context.read())),
   RepositoryProvider<DeleteUserUseCase>(create: (context) => DeleteUserUseCase(context.read())),
   RepositoryProvider<GetActivitiesUseCase>(create: (context) => GetActivitiesUseCase(context.read())),
+  RepositoryProvider<UpsertUserUseCase>(create: (context) => UpsertUserUseCase(context.read())),
+  RepositoryProvider<GetCountriesUseCase>(create: (context) => GetCountriesUseCase()),
 ];
