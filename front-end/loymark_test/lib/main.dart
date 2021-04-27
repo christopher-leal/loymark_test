a70ec33952175db99ce1ff2a9b47b6d690fbf7d4
Christopher Leal <chris_t_o@hotmail.com>
2021-04-26 21:27:32 -0500

    countries catalog added

--- front-end/loymark_test/lib/main.dart
+++ front-end/loymark_test/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:loymark_test/dependencies.dart';
+import 'package:loymark_test/presentation/pages/home/countries_cubit.dart';
 import 'package:loymark_test/presentation/pages/home/home_screen.dart';
 
 void main() => runApp(MyApp());
@@ -11,18 +12,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
       providers: buildRepositories,
-      child: MaterialApp(
-        title: 'Loymark',
-        debugShowCheckedModeBanner: false,
-        localizationsDelegates: const [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: const [
-          Locale('es', 'ES'),
-        ],
-        home: HomeScreen(),
+      child: BlocProvider(
+        lazy: false,
+        create: (context) => CountriesCubit(context.read())..getCountries(),
+        child: MaterialApp(
+          title: 'Loymark',
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('es', 'ES'),
+          ],
+          home: HomeScreen(),
+        ),
       ),
     );
   }
